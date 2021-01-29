717bf9b2bbc112fb103dd34126982bc0060efe71
Nick Reiss <nreiss45@gmail.com>
2021-01-29 11:14:29 -0600

    Update Cleaner API.user.js

--- Cleaner API.user.js
+++ Cleaner API.user.js
@@ -384,7 +384,7 @@ var RunSave = false;
 			var count = 0;
 			for(var i = 0; i < 25; i++){
 				if(stateOfButtons[i]){
-					$('title').text(SroNum + "5" + buttonNumber);
+					$('title').text(SroNum + " - Line " + buttonNumber);
 					count++;
 				}
 			}
